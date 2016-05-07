f88e88d8d93e6a354057eccd153468124fd0295d
Stefan Kanev <stefan.kanev@gmail.com>
2016-05-07 12:05:17 +0300

    Add tmux with 24-bit color support

---
+++ tmux.rb
@@ -0,0 +1,51 @@
+# TMUX with 24-bit color support.  Lifted from:
+#
+#   https://github.com/choppsv1/homebrew-term24
+#
+# I only need this, because tmux 2.2 has some issue on Mac OS X and they won't
+# put it in homebrew.  Once 2.3 is out, this should work out of the box.
+class Tmux < Formula
+  desc "Terminal multiplexer"
+  homepage "https://tmux.github.io/"
+  url "https://github.com/tmux/tmux/releases/download/2.1/tmux-2.1.tar.gz"
+  sha256 "31564e7bf4bcef2defb3cb34b9e596bd43a3937cad9e5438701a81a5a9af6176"
+
+  head do
+    url "https://github.com/tmux/tmux.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  patch :p1 do
+    url "https://gist.githubusercontent.com/choppsv1/352421a9f49262ad5c93/raw/5d29be026e11f75a9f570f354149c913c749a699/tmux-2.1-24bit.diff"
+    sha256 "6d92559da2bf7433f505efd785404fe055772c7cd8c49d208d7522b169c09903"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+
+  def install
+    system "sh", "autogen.sh" if build.head?
+
+    ENV.append "LDFLAGS", "-lresolv"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}"
+    system "make", "install"
+
+    bash_completion.install "examples/bash_completion_tmux.sh" => "tmux"
+    pkgshare.install "examples"
+  end
+
+  def caveats; <<-EOS.undent
+    Example configurations have been installed to:
+      #{pkgshare}/examples
+    EOS
+  end
+
+  test do
+    system "#{bin}/tmux", "-V"
+  end
+end
